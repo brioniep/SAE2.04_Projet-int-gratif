da0322fb1f1d44d0f9d8f80833b84e488b477d26
MacheG0ule <147591755+MacheG0ule@users.noreply.github.com>
2024-06-19 14:37:44 +0200

    Add files via upload

---
+++ integratif_v1/monappli/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path 
+from . import views
+
+urlpatterns = [
+    #page d'accueil
+    path("index/", views.index),
+
+    #page capteurs
+    path("index-capteurs/", views.indexcapteurs),
+    path("affiche-capteurs", views.affichecapteurs),
+    path("update-capteurs",views.updatecapteurs),
+    path("updatetraitement-capteurs/<int:id>/", views.updatetraitementcapteurs),
+    
+    # page données
+    path("index-donnees/", views.indexdonnees),
+    path("affiche-donnees", views.affichedonnees),
+]
